8786a524c8d0d7ed1cac4acb061c9e74c10e918d
Rohit kumar <143171477+rohitkumar220944@users.noreply.github.com>
2025-02-18 23:06:25 +0530

    Add files via upload

---
+++ sumelement_two_array.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>arr1{0,0,5,4,1,7};
+    vector<int>arr2{1,3,2,5};
+    vector<int>ans;
+    int carry=0;
+    int i=arr1.size()-1;
+    int j=arr2.size()-1;
+    while(i>=0 && j>=0)
+    {
+        int x=arr1[i]+arr2[j]+carry;
+        int digit=x%10;
+        ans.push_back(digit);
+        carry=x/10;
+        i--;
+        j--;
+    }
+     while(i>=0)
+    {
+        int x=arr1[i]+0+carry;
+        int digit=x%10;
+        ans.push_back(digit);
+    carry=x/10;
+        i--;
+    }
+    
+    while(j>=0)
+    {
+        int x=arr1[j]+0+carry;
+        int digit=x%10;
+        ans.push_back(digit);
+        carry=x/10;
+        j--;
+    }
+    if(carry)
+    {
+        ans.push_back(carry);
+    }
+    while(ans[ans.size()-1]==0)
+    {
+        ans.pop_back();
+    }
+    reverse(ans.begin(),ans.end());
+    for(int i=0;i<ans.size();i++){
+    cout<<ans[i];
+    }
+    
+    
+}
